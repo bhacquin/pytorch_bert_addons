00f12703775d92ae940088eb0440806bd3f854cc
Bastien <bastien.van-delft@unchartech.com>
2019-05-14 11:17:46 +0200

    Modification d'une erreur dans run_asw.sh .

--- pytorch-pretrained-BERT/run_aws.sh
+++ pytorch-pretrained-BERT/run_aws.sh
@@ -32,10 +32,7 @@ case $1 in
   --word_file)word_file="$2" ; shift;;
   --output_file)output_file="$2" ; shift;;
   --threshold)threshold="$2" ; shift;;
-  (--) shift; break;;
-  (-*) echo "$0: error - unrecognized option $1" 1>&2 exit1;;
-  (*) break ;;
-  -a| --all)pip install -r requirements.txt
+  -a| --all) pip install -r requirements.txt
 python -m spacy download en_core_web_lg
 mkdir data
 mkdir test
@@ -43,7 +40,6 @@ wget https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-vocab
 $address = pytorch_pretrained_bert
 chmod +x $address/1_extract_vocab.sh
 # bash 1_extract_vocab.sh annotated_stuff.txt
-
 echo "Vocabulary issues"
 # python 2_find_missing_tokens.py --vocab_file bert-base-uncased-vocab.txt > new_vocab.txt
 # python python1_data.py
@@ -51,17 +47,17 @@ echo "Vocabulary issues"
 # mv full_vocab.txt vocab.txt
 # python python2_data.py
 python $address/vocab_treatment.py
-
 echo "Prepare text data"
 python $address/prepare_all_texts_for_bert.py --input_folder data/ --ouput_file training_text.txt
 python $address/prepare_all_texts_for_bert.py --input_folder test/ --ouput_file test_text.txt
 rm -r training/
 rm -r test/
-
 echo 'generating data for train'
 python $address/pregenerate_training_data.py --train_corpus 'training_text.txt' --bert_model vocab.txt --do_lower_case --output_dir training/ --epochs_to_generate 2 --max_seq_len 512
-python $address/pregenerate_training_data.py --train_corpus 'test_text.txt' --bert_model vocab.txt --do_lower_case --output_dir test/ --epochs_to_generate 2 --max_seq_len 512
-;;
+python $address/pregenerate_training_data.py --train_corpus 'test_text.txt' --bert_model vocab.txt --do_lower_case --output_dir test/ --epochs_to_generate 2 --max_seq_len 512;;
+(--) shift; break;;
+(-*) echo "$0: error - unrecognized option $1" 1>&2 exit1;;
+(*) break ;;
 
 esac
 shift
