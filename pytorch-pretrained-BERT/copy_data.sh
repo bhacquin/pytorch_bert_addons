5f72c5cb110df3d14be67911bb1d4136bc918789
Bastien <bastien.van-delft@unchartech.com>
2019-05-14 16:59:03 +0200

    Ajout de code pour coder la data importer avec scp

---
+++ pytorch-pretrained-BERT/copy_data.sh
@@ -0,0 +1,5 @@
+mkdir ~/pytorch_bert_addons/pytorch-pretrained-BERT/data/
+cp ~/data/annotated_stuff.txt ~/pytorch_bert_addons/pytorch-pretrained-BERT/data/
+cp ~/data/annotated_stuff.json ~/pytorch_bert_addons/pytorch-pretrained-BERT/data/
+cp ~/data/transcripts_presentation.txt ~/pytorch_bert_addons/pytorch-pretrained-BERT/data/
+cp ~/data/bert-base-uncased-vocab.txt ~/pytorch_bert_addons/pytorch-pretrained-BERT/data/
