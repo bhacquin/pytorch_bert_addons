62f6fc1583405a222ca83a0fd3de632d255103d3
bhacquin <bastien.van-delft@unchartech.com>
2019-05-21 18:32:28 +0200

    Distributed cuda set backend and bash settings.

--- pytorch-pretrained-BERT/distributed_setting.sh
+++ pytorch-pretrained-BERT/distributed_setting.sh
@@ -1,8 +1,16 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y linux-aws
+sudo reboot
+sudo apt-get install -y gcc make linux-headers-$(uname -r)
+
 source activate pytorch_p36
 pip uninstall torch
 pip install torch-nightly -f https://download.pytorch.org/whl/nightly/cu90/torch_nightly.html
 git clone https://github.com/pytorch/vision.git
 cd vision && python setup.py install
 cd ..
-ifconfig
-echo 'export NCCL_SOCKET_IFNAME=ens3 ?'
+ifconfig &
+export NCCL_SOCKET_IFNAME='ens3'
+export MASTER_ADDR=
+export MASTER_PORT=
+export WORLD_SIZE=
