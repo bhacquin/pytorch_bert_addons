91cb2a27851ede91c04bab833f745b3fd306a4ef
bhacquin <bastien.van-delft@unchartech.com>
2019-09-25 15:08:04 +0200

    Retrait du classifier

--- pytorch-pretrained-BERT/pytorch_pretrained_bert/modeling.py
+++ pytorch-pretrained-BERT/pytorch_pretrained_bert/modeling.py
@@ -1033,15 +1033,15 @@ class BertForSequenceClassification(BertPreTrainedModel):
         self.num_labels = num_labels
         self.bert = BertModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.classifier = nn.Sequential(nn.Linear(config.hidden_size, config.hidden_size),nn.Tanh())
-        self.last_layer = nn.Linear(config.hidden_size, num_labels)
+        self.classifier = nn.Linear(config.hidden_size, cself.num_labels)
+
         self.apply(self.init_bert_weights)
 
     def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None):
         _, pooled_output = self.bert(input_ids, token_type_ids, attention_mask, output_all_encoded_layers=False)
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
-        logits= self.last_layer(logits)
+
 
         if labels is not None:
             loss_fct = CrossEntropyLoss()
