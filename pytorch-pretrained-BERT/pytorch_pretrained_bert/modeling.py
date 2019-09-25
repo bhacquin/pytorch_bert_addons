2dec6e99b08c373a2136a9ab55e3caf6afea8036
bhacquin <bastien.van-delft@unchartech.com>
2019-09-25 15:18:21 +0200

    Retrait du classifier

--- pytorch-pretrained-BERT/pytorch_pretrained_bert/modeling.py
+++ pytorch-pretrained-BERT/pytorch_pretrained_bert/modeling.py
@@ -1033,7 +1033,7 @@ class BertForSequenceClassification(BertPreTrainedModel):
         self.num_labels = num_labels
         self.bert = BertModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.classifier = nn.Linear(config.hidden_size, cself.num_labels)
+        self.classifier = nn.Linear(config.hidden_size, self.num_labels)
 
         self.apply(self.init_bert_weights)
 
